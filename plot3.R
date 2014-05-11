0fbda27e94d9480d0b1503579a5d040c283c856b
ThisIsACourseraHomeworkSite <volotr7@yahoo.com>
2014-05-11 17:10:39 -0400

    git config --global user.email "volotr7@yahoo.com" git config --global user.password "kingof97"
    Signed-off-by: ThisIsACourseraHomeworkSite <volotr7@yahoo.com>

--- plot3.R
+++ plot3.R
@@ -1,6 +1,6 @@
 # open data and label
 classes = c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric")
-power = read.table(file = "household_power_consumption.csv", sep = ';', colClasses = classes, header= TRUE, na.strings="?")
+power = read.table(file = "E:/Coursera/RCompData/household_power_consumption.csv", sep = ';', colClasses = classes, header= TRUE, na.strings="?")
 
 # use only data for dates 2007-02-01 and 2007-02-02
 pow = power[power$Date == "1/2/2007" | power$Date == "2/2/2007",]
@@ -12,8 +12,8 @@ plot(pow$datetime, pow$Sub_metering_1, xlab="", ylab = "Energy sub metering", co
 lines(pow$datetime, pow$Sub_metering_1, col = "black")
 lines(pow$datetime, pow$Sub_metering_2, col = "red")
 lines(pow$datetime, pow$Sub_metering_3, col = "blue")
-legend("topright", legend = c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"), 
-       lwd = c(2, 2, 2), col=c("black", "red", "blue"), cex = 1.5,
+legend("topright", legend = c("Sub_metering_1","Sub_metering_2             ", "Sub_metering_3             "), 
+       lwd = c(2, 2, 2), col=c("black", "red", "blue"), cex = 1,
        lty = c(1,1,1))
 dev.copy(png, file = "plot3.png", width = 480, height = 480)
 dev.off()
